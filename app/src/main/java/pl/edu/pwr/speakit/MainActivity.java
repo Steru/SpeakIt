b31dc7c7aa01ac71f865b5bc2c98270185c2231f
Steru <s.stergiadis@gmail.com>
2017-01-29 16:57:25 +0100

    Add play music command
    Based on a string, from song list most similar are withdrawn

--- app/src/main/java/pl/edu/pwr/speakit/MainActivity.java
+++ app/src/main/java/pl/edu/pwr/speakit/MainActivity.java
@@ -1,11 +1,15 @@
 package pl.edu.pwr.speakit;
 
+import android.Manifest;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.speech.RecognizerIntent;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,6 +24,7 @@ import java.util.Locale;
 
 import pl.edu.pwr.speakit.commands.CallCommand;
 import pl.edu.pwr.speakit.commands.LaunchAppCommand;
+import pl.edu.pwr.speakit.commands.PlayMusicCommand;
 import pl.edu.pwr.speakit.commands.SmsCommand;
 import pl.edu.pwr.speakit.common.CommandDO;
 import pl.edu.pwr.speakit.common.CommandGeneratorThread;
@@ -42,8 +47,33 @@ public class MainActivity extends AppCompatActivity implements IAsyncMorfeuszRes
 
         mRecognizedTextView = (TextView) findViewById(R.id.recognized_text);
         mTelephoneNumber = (EditText) findViewById(R.id.telephone_number_edit_text);
+
+        checkPermissions();
+
     }
 
+    private void checkPermissions() {
+        //ugly way, cause it wont wait for a request, but what the hell
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.INTERNET)
+                        != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.ACCESS_NETWORK_STATE)
+                        != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.CALL_PHONE,
+                            Manifest.permission.INTERNET,
+                            Manifest.permission.ACCESS_NETWORK_STATE,
+                            Manifest.permission.READ_EXTERNAL_STORAGE},
+                    0);
+        }
+    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -103,6 +133,12 @@ public class MainActivity extends AppCompatActivity implements IAsyncMorfeuszRes
             }
         };
         launchThread.start();
+    }
+
+    public void playSpecificMusic(View view){
+        PlayMusicCommand playMusicCommand = new PlayMusicCommand(this);
+        playMusicCommand.playSpecificSong("song");
+
     }
 
     @Override
---
+++ app/src/main/java/pl/edu/pwr/speakit/commands/PlayMusicCommand.java
@@ -0,0 +1,145 @@
+package pl.edu.pwr.speakit.commands;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import net.ricecode.similarity.JaroWinklerStrategy;
+import net.ricecode.similarity.StringSimilarityService;
+import net.ricecode.similarity.StringSimilarityServiceImpl;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import pl.edu.pwr.speakit.R;
+
+/**
+ * Created by Steru on 2017-01-28.
+ */
+public class PlayMusicCommand {
+    private static String TAG = "PlayMusicCommand";
+    private final Context mContext;
+    private List<Song> mSongList = new ArrayList();
+    private Song mChosenOne;
+
+    public PlayMusicCommand(Context context) {
+        mContext = context;
+    }
+
+    public void playSpecificSong(String phrase) {
+        matchSongTitle(phrase, getAllSongs());
+        if (mSongList.isEmpty()) {
+            Toast.makeText(mContext, R.string.song_not_found, Toast.LENGTH_LONG).show();
+        } else if (mSongList.size() == 1) {
+            mChosenOne = mSongList.get(0);
+            launchPlayingIntent();
+        } else {
+            showDialogChooser();
+        }
+    }
+
+    private void launchPlayingIntent() {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        File file = new File(mChosenOne.path);
+        intent.setDataAndType(Uri.fromFile(file), "audio/*");
+        mContext.startActivity(intent);
+    }
+
+    private void showDialogChooser() {
+        ArrayAdapter<Song> arrayAdapter = new ArrayAdapter<>(mContext,
+                android.R.layout.select_dialog_singlechoice);
+
+        for (Song s : mSongList){
+            arrayAdapter.add(s);
+        }
+
+        AlertDialog.Builder builderSingle = new AlertDialog.Builder(mContext);
+        builderSingle.setTitle(R.string.select_song);
+
+        builderSingle.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builderSingle.setAdapter(arrayAdapter, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //String strName = mSongList.get(which).toString();
+                mChosenOne = mSongList.get(which);
+                Log.d(TAG, "Chosen -> " + mChosenOne);
+                launchPlayingIntent();
+            }
+        });
+        builderSingle.show();
+    }
+
+    private void matchSongTitle(String phrase, List<Song> allSongs) {
+        StringSimilarityService similarityService =
+                new StringSimilarityServiceImpl(new JaroWinklerStrategy());
+
+        for (Song s : allSongs){
+            double titleSimilarity  = similarityService.score(phrase, s.title);
+            double artistSimilarity = similarityService.score(phrase, s.artist);
+            if(titleSimilarity > 0.8 || artistSimilarity > 0.8) {
+                Log.d(TAG, "It's a match!");
+                mSongList.add(s);
+            }
+        }
+    }
+
+    private List<Song> getAllSongs() {
+        List<Song> allSongs = new ArrayList<>();
+        ContentResolver musicResolver = mContext.getContentResolver();
+        Uri musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
+        if (musicCursor != null && musicCursor.moveToFirst()) {
+            int titleColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.TITLE);
+            int idColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media._ID);
+            int artistColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.ARTIST);
+            int pathColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.DATA);
+
+            do {
+                long thisId         = musicCursor.getLong(idColumn);
+                String thisTitle    = musicCursor.getString(titleColumn);
+                String thisArtist   = musicCursor.getString(artistColumn);
+                String thisPath     = musicCursor.getString(pathColumn);
+                allSongs.add(new Song(thisId, thisTitle, thisArtist, thisPath));
+            }
+            while (musicCursor.moveToNext());
+        }
+        return allSongs;
+    }
+
+    class Song {
+        public long id;
+        public String title;
+        public String artist;
+        public String path;
+
+        public Song(long songID, String songTitle, String songArtist, String thisPath) {
+            id = songID;
+            title = songTitle;
+            artist = songArtist;
+            path = thisPath;
+        }
+
+        @Override
+        public String toString() {
+            return artist + " - " + title;
+        }
+    }
+}
